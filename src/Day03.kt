2f27afbbc80ea33db83edf74092c434407510859
Mathieu Larue <mat_1794@hotmail.com>
2023-12-04 21:54:34 -0500

    Part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,14 +1,19 @@
 import java.io.File
 
+private data class Data(
+    val numbers: Map<Pair<Int, Int>, String>,
+    val symbols: Set<Pair<Pair<Int, Int>, Char>>
+)
+
 fun main() {
     val lines = File("Day03Input.txt").readLines()
     println("Part 1 result: ${part1(lines)}")
+    println("Part 2 result: ${part2(lines)}")
 }
 
-private fun part1(lines: List<String>): Int {
+private fun parseData(lines: List<String>): Data {
     val numbers = mutableMapOf<Pair<Int, Int>, String>()
-    val symbols = mutableSetOf<Pair<Int, Int>>()
-    val lineLength = lines[0].length
+    val symbols = mutableSetOf<Pair<Pair<Int, Int>, Char>>()
 
     lines.forEachIndexed { y, line ->
         var currentNumber = ""
@@ -35,7 +40,7 @@ private fun part1(lines: List<String>): Int {
                 startX = null
 
                 if (line[x] != '.') {
-                    symbols.add(Pair(y, x))
+                    symbols.add((y to x) to line[x])
                 }
             }
         }
@@ -43,42 +48,76 @@ private fun part1(lines: List<String>): Int {
         addToNumbersIfNecessary()
     }
 
-    return numbers.map { (coords, number) ->
-        val projections = mutableSetOf<Pair<Int, Int>>()
-        val (y, startX) = coords
+    return Data(numbers, symbols)
+}
 
-        number.indices.forEach { i ->
-            val x = startX + i
+private fun Map.Entry<Pair<Int, Int>, String>.calculateProjections(lines: List<String>): Pair<Set<Pair<Int, Int>>, String> {
+    val lineLength = lines[0].length
+    val projections = mutableSetOf<Pair<Int, Int>>()
+    val (y, startX) = key
+    val number = value
 
-            if (x - 1 >= 0 && i == 0) { // left
-                projections += Pair(y, x - 1)
-            }
-            if (x + 1 <= lineLength - 1 && i == number.indices.last) { // right
-                projections += Pair(y, x + 1)
-            }
-            if (y - 1 >= 0) { // top
-                projections += Pair(y - 1, x)
-            }
-            if (y + 1 <= lines.size - 1) { // bottom
-                projections += Pair(y + 1, x)
-            }
+    number.indices.forEach { i ->
+        val x = startX + i
 
-            if (x - 1 >= 0 && i == 0 && y - 1 >= 0) { // top left
-                projections += Pair(y - 1, x - 1)
-            }
-            if (x + 1 <= lineLength - 1 && y - 1 >= 0) { // top right
-                projections += Pair(y - 1, x + 1)
-            }
-            if (x - 1 >= 0 && i == 0 && y + 1 <= lines.size - 1) { // bottom left
-                projections += Pair(y + 1, x - 1)
-            }
-            if (x + 1 <= lineLength - 1 && i == number.indices.last && y + 1 <= lines.size - 1) { // bottom right
-                projections += Pair(y + 1, x + 1)
-            }
+        if (x - 1 >= 0 && i == 0) { // left
+            projections += Pair(y, x - 1)
+        }
+        if (x + 1 <= lineLength - 1 && i == number.indices.last) { // right
+            projections += Pair(y, x + 1)
+        }
+        if (y - 1 >= 0) { // top
+            projections += Pair(y - 1, x)
+        }
+        if (y + 1 <= lines.size - 1) { // bottom
+            projections += Pair(y + 1, x)
         }
 
-        number.toInt() to projections
+        if (x - 1 >= 0 && i == 0 && y - 1 >= 0) { // top left
+            projections += Pair(y - 1, x - 1)
+        }
+        if (x + 1 <= lineLength - 1 && y - 1 >= 0) { // top right
+            projections += Pair(y - 1, x + 1)
+        }
+        if (x - 1 >= 0 && i == 0 && y + 1 <= lines.size - 1) { // bottom left
+            projections += Pair(y + 1, x - 1)
+        }
+        if (x + 1 <= lineLength - 1 && i == number.indices.last && y + 1 <= lines.size - 1) { // bottom right
+            projections += Pair(y + 1, x + 1)
+        }
     }
-        .filter { it.second.intersect(symbols).isNotEmpty() }
-        .sumOf { it.first }
+
+    return projections to number
+}
+
+private fun part1(lines: List<String>): Int {
+    val data = parseData(lines)
+    val symbolsCoords = data.symbols.map { (coords, _) -> coords }.toSet()
+
+    return data.numbers
+        .map { it.value.toInt() to it.calculateProjections(lines) }
+        .filter { (_, projections) ->
+            val coordsSet = projections.first
+            coordsSet.intersect(symbolsCoords).isNotEmpty()
+        }
+        .sumOf { (number, _) -> number }
+}
+
+private fun part2(lines: List<String>): Int {
+    val (numbers, symbols) = parseData(lines)
+
+    val projections = numbers.map {
+        it.calculateProjections(lines)
+    }
+
+    return symbols
+        .filter { (_, symbol) -> symbol == '*' }
+        .map { (coords, _) -> coords }
+        .mapNotNull { coords ->
+            projections
+                .filter { (coordsSet, _) -> coordsSet.contains(coords) }
+                .takeIf { it.size == 2 }
+                ?.map { (_, number) -> number.toInt() }
+        }
+        .sumOf { it.reduce { acc, s -> acc * s } }
 }
