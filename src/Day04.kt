73958fe714c4c4c2aae77e445c8a1d7a85b7c6fa
Mathieu Larue <mlarue@mirego.com>
2023-12-09 09:30:07 -0500

    Day 4 part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -3,17 +3,47 @@ import kotlin.math.pow
 
 fun main() {
     val lines = File("Day04Input.txt").readLines()
-    println("Part 1 result: ${part1(lines)}")
+    val data = parseData(lines)
+
+    println("Part 1 result: ${part1(data)}")
+    println("Part 2 result: ${part2(data)}")
 }
 
-private fun part1(lines: List<String>): Int = lines.map { line ->
-    val parts = line.split(": ")[1].split(" | ")
-    val winning = parts[0].chunked(3).map { it.trim().toInt() }.toSet()
-    val other = parts[1].chunked(3).map { it.trim().toInt() }.toSet()
+private fun parseData(lines: List<String>): Map<Int, Pair<Set<Int>, Set<Int>>> =
+    lines.associate { line ->
+        val gamePart = line.split(": ")
+        val cardNumber = gamePart[0].removePrefix("Card").trim().toInt()
+        val parts = gamePart[1].split(" | ")
+        val winning = parts[0].chunked(3).map { it.trim().toInt() }.toSet()
+        val other = parts[1].chunked(3).map { it.trim().toInt() }.toSet()
 
-    other.intersect(winning)
+        cardNumber to Pair(winning, other)
+    }
+
+private fun part1(data: Map<Int, Pair<Set<Int>, Set<Int>>>): Int = data.map { entry ->
+    entry.value.second.intersect(entry.value.first)
         .size
         .takeIf { it > 0 }
         ?.let { winningNumbersCount -> (1 * 2.0.pow(winningNumbersCount.toDouble() - 1)).toInt() }
         ?: 0
 }.sum()
+
+private fun part2(data: Map<Int, Pair<Set<Int>, Set<Int>>>): Int {
+    val copies: MutableMap<Int, Int> = data.entries.associate { it.key to 1 }.toMutableMap()
+    data.map { entry ->
+        val winningCount = entry.value.second.intersect(entry.value.first).size
+        val numberOfCardsForCurrentEntry = copies.getValue(entry.key)
+
+        repeat(numberOfCardsForCurrentEntry) {
+            val cardNumbersToIncrement = entry.key + 1..entry.key + winningCount
+
+            cardNumbersToIncrement.forEach { cardNumberToIncrement ->
+                if (copies.containsKey(cardNumberToIncrement)) {
+                    copies[cardNumberToIncrement] = copies.getValue(cardNumberToIncrement) + 1
+                }
+            }
+        }
+    }
+
+    return copies.values.sum()
+}
