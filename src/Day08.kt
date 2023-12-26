08b6113cc31b809073f0ded3cba8e0fc9db5cc1c
Mathieu Larue <mlarue@mirego.com>
2023-12-26 14:52:20 -0500

    Day 8 part 2

--- src/Day08.kt
+++ src/Day08.kt
@@ -19,6 +19,7 @@ fun main() {
         }
 
     println("Part 1 result: ${part1(instructions, network)}")
+    println("Part 2 result: ${part2(instructions, network)}")
 }
 
 private fun part1(instructions: String, network: Map<String, Pair<String, String>>): Int {
@@ -28,12 +29,12 @@ private fun part1(instructions: String, network: Map<String, Pair<String, String
 
     do {
         val instruction = instructions[i]
-        val node = network.getValue(currentKey)
+        val destinations = network.getValue(currentKey)
 
         currentKey = if (instruction == 'R') {
-            node.second
+            destinations.second
         } else {
-            node.first
+            destinations.first
         }
 
         ++i
@@ -44,4 +45,62 @@ private fun part1(instructions: String, network: Map<String, Pair<String, String
     } while (currentKey != "ZZZ")
 
     return steps
+}
+
+private fun part2(instructions: String, network: Map<String, Pair<String, String>>): Long {
+    val steps = network
+        .keys
+        .filter { it.endsWith("A") }
+        .map { keyToMap ->
+            var steps = 0L
+            var i = 0
+            var currentKey = keyToMap
+
+            do {
+                val instruction = instructions[i]
+                val destinations = network.getValue(currentKey)
+
+                currentKey = if (instruction == 'R') {
+                    destinations.second
+                } else {
+                    destinations.first
+                }
+
+                ++i
+                ++steps
+                if (i >= instructions.length) {
+                    i = 0
+                }
+            } while (!currentKey.endsWith("Z"))
+
+            steps
+        }
+
+    return leastCommonMultiple(steps.toLongArray())
+}
+
+private fun greatestCommonDivider(a: Long, b: Long): Long {
+    var currentA = a
+    var currentB = b
+
+    while (currentB > 0) {
+        val temp = currentB
+        currentB = currentA % currentB
+        currentA = temp
+    }
+
+    return currentA
+}
+
+private fun leastCommonMultiple(a: Long, b: Long): Long =
+    a * (b / greatestCommonDivider(a, b))
+
+private fun leastCommonMultiple(input: LongArray): Long {
+    var result = input[0]
+
+    for (i in 1 until input.size) {
+        result = leastCommonMultiple(result, input[i])
+    }
+
+    return result
 }
