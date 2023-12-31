6a6a6470dba484ff4bff030802edd0d8d104ddf8
Mathieu Larue <mlarue@mirego.com>
2023-12-31 17:28:31 -0500

    Day 9 part 1

---
+++ src/Day09.kt
@@ -0,0 +1,38 @@
+import java.io.File
+
+fun main() {
+    val lines = File("Day09Input.txt").readLines()
+    val sequences = lines
+        .map { line ->
+            line
+                .split(" ")
+                .map { it.toInt() }
+        }
+
+    val sequencesWithDifferences = sequences.map { sequence ->
+        val differences = mutableListOf<List<Int>>()
+        var current = sequence
+
+        do {
+            val diff = current
+                .zipWithNext()
+                .map { it.second - it.first }
+            differences.add(diff)
+            current = diff
+        } while (!current.all { it == 0 })
+
+        listOf(sequence) + differences
+    }
+
+    println("Part 1 result: ${part1(sequencesWithDifferences)}")
+}
+
+private fun part1(sequencesWithDifferences: List<List<List<Int>>>): Int =
+    sequencesWithDifferences
+        .map { sequenceWithDifferences ->
+            sequenceWithDifferences.reduceRightIndexed { index, differences, acc ->
+                val toAppend: Int = differences.last() + if (index == sequenceWithDifferences.lastIndex - 1) 0 else acc.last()
+                differences + listOf(toAppend)
+            }
+        }
+        .sumOf { it.last() }
